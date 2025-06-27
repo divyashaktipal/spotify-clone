8b165f112a678dc382b7c0ffb541e9e15fa4437f
Divyashakti pal <divyashaktipal@gmail.com>
2025-06-27 09:16:21 +0530

    Update DownloadApp.js

--- src/components/Sidebar/DownloadApp.js
+++ src/components/Sidebar/DownloadApp.js
@@ -4,7 +4,7 @@ function DownloadApp() {
 	return (
 		<a href="#" className="h-10 flex flex-shrink-0 text-sm font-semibold text-link hover:text-white gap-x-4 items-center px-6">
 			<Icon name="download" size={20} />
-			Uygulamayı Yükle
+			Divyashakti
 		</a>
 	)
 }
